634a68f3cec0a03193f0647c5997bec777bdbc02
embadmin <embadmin@emb-ban-300.embitel.local>
2019-04-02 12:20:01 +0530

    Some more changes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Automating the react build
+            Automating the react build once again.
 
           </a>
         </header>
